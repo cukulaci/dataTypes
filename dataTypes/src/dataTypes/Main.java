dcd8137d0987101c32356972be66eeecc3bc1295
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2022-09-29 08:43:23 +0300

    Add files via upload

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,21 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Primitive Types
+		
+		int sayi = 12;
+		double sayiOndalik = 12.2;
+		char character ='A';
+		boolean isItTrue = true;
+		
+		System.out.println(sayi);
+		System.out.println(sayiOndalik);
+		System.out.println(character);
+		System.out.println(isItTrue);
+		
+	}
+
+}
